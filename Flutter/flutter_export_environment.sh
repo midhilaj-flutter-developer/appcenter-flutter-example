f90722ba6f4efa9868425d9a60060346908170f6
Midhilaj M <midhilajm@exalture.com>
2019-10-30 12:37:41 +0530

    First commit

---
+++ Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/project/ide/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/exalture/AndroidStudioProjects/fluttertry2/flutter_app2"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/project/ide/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
